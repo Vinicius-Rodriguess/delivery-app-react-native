f33b400182c8416abf43ed84a05d7118ce3422a5
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2025-05-02 16:03:14 -0300

    :sparkles: feat: Adicionado componente Trending

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,6 +5,7 @@ import Constants from "expo-constants"
 import { Banner } from "../components/banner";
 import { Search } from "../components/search";
 import { Section } from "../components/section";
+import { TrendingFoods } from "../components/trending";
 
 const statusBarHeight = Constants.statusBarHeight
 
@@ -23,12 +24,14 @@ export default function Index() {
       <Search/>
     </View>
 
-    <Section
+      <Section
         name="Comidas em alta"
         label="Veja mais"
         action={ () => console.log("CLICOU NO VEJA MAIS")}
         size="text-2xl"
       />
+      
+      <TrendingFoods/>
 
       <Section
         name="Famosos no DevFood"
