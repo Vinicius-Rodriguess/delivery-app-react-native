697c6e09f124b1707029a1d4f6568197178fa707
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2025-05-02 16:23:36 -0300

    :sparkles: feat: Adicionado componente List

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,6 +7,7 @@ import { Search } from "../components/search";
 import { Section } from "../components/section";
 import { TrendingFoods } from "../components/trending";
 import { Restaurants } from "../components/restaurants";
+import { RestaurantVerticalList } from "../components/list";
 
 const statusBarHeight = Constants.statusBarHeight
 
@@ -49,6 +50,8 @@ export default function Index() {
         action={ () => console.log("CLICOU NO RESTAURANTES")}
         size="text-xl"
       />
+
+      <RestaurantVerticalList/>
 
     </ScrollView>
   );
